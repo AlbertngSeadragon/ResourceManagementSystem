09b987deec0c927f9d341e17ab728e596f374051
albertng <albertng412@gmail.com>
2022-02-11 22:24:37 +0800

    Backend Get Request

--- backend/mysqldb.py
+++ backend/mysqldb.py
@@ -23,45 +23,45 @@ def Jsonifly(rows):
     # print(data)
     return data
 
-def returnprojects(id):
-    try:
-        mycursor = mydb.cursor()
+# def returnprojects(id):
+#     try:
+#         mycursor = mydb.cursor()
 
-        mycursor.execute(f"select * from project.projects where id={id}")
+#         mycursor.execute(f"select * from project.projects where id={id}")
 
-        promisefuturedata = Jsonifly.remote(mycursor.fetchall())
+#         promisefuturedata = Jsonifly.remote(mycursor.fetchall())
 
-        jsondata = ray.get(promisefuturedata)
+#         jsondata = ray.get(promisefuturedata)
 
-        jsondata = {'projects': jsondata}
+#         jsondata = {'projects': jsondata}
 
-        jsondata = json.dumps(jsondata)
+#         jsondata = json.dumps(jsondata)
 
-        print(jsondata)
+#         print(jsondata)
 
-        return jsondata
-    except:
-        print("Database Error")
+#         return jsondata
+#     except:
+#         print("Database Error")
 
-def returnotherprojects(id):
-    try:
-        mycursor = mydb.cursor()
+# def returnotherprojects(id):
+#     try:
+#         mycursor = mydb.cursor()
 
-        mycursor.execute(f"select * from project.other_projects where id={id}")
+#         mycursor.execute(f"select * from project.other_projects where id={id}")
 
-        promisefuturedata = Jsonifly.remote(mycursor.fetchall())
+#         promisefuturedata = Jsonifly.remote(mycursor.fetchall())
 
-        jsondata = ray.get(promisefuturedata)
+#         jsondata = ray.get(promisefuturedata)
 
-        jsondata = {'projects': jsondata}
+#         jsondata = {'projects': jsondata}
 
-        jsondata = json.dumps(jsondata)
+#         jsondata = json.dumps(jsondata)
 
-        print(jsondata)
+#         print(jsondata)
 
-        return jsondata
-    except:
-        print("Database Error")
+#         return jsondata
+#     except:
+#         print("Database Error")
 
 def returnExpenseGroups():
     try:
@@ -94,42 +94,42 @@ def returnExpenseGroups():
     except:
         print("Database Error3")
 
-def returnProjects():
-    try:
-        mycursor = mydb.cursor()
+# def returnProjects():
+#     try:
+#         mycursor = mydb.cursor()
 
-        mycursor.execute(f"select id, projectName, initialBalance, start_time, end_time, bgColor from test_projects_balance_expense.Projects")
+#         mycursor.execute(f"select id, projectName, initialBalance, start_time, end_time, bgColor from test_projects_balance_expense.Projects")
 
-        promisefuturedata = Jsonifly.remote(mycursor.fetchall())
+#         promisefuturedata = Jsonifly.remote(mycursor.fetchall())
 
-        jsondata = ray.get(promisefuturedata)
+#         jsondata = ray.get(promisefuturedata)
 
-        # jsondata = {'Projects': jsondata}
+#         # jsondata = {'Projects': jsondata}
 
-        # jsondata = json.dumps(jsondata)
+#         # jsondata = json.dumps(jsondata)
 
-        # print(jsondata)
+#         # print(jsondata)
 
-        # return jsondata
+#         # return jsondata
 
-        print(jsondata)
+#         print(jsondata)
 
-        append_json = []
-        for row in jsondata:
-            jsondata1 = {"id":row[0], "projectName":row[1], "initialBalance":row[2], "start_time":row[3], "end_time":row[4], "bgColor":row[5]}
-            append_json.append(jsondata1)
-        ## jsondata = {'ExpenseGroups': jsondata}
+#         append_json = []
+#         for row in jsondata:
+#             jsondata1 = {"id":row[0], "projectName":row[1], "initialBalance":row[2], "start_time":row[3], "end_time":row[4], "bgColor":row[5]}
+#             append_json.append(jsondata1)
+#         ## jsondata = {'ExpenseGroups': jsondata}
 
-        ##jsondata1 = json.dumps(jsondata1)
+#         ##jsondata1 = json.dumps(jsondata1)
 
-        ##print(append_json)
+#         ##print(append_json)
 
-        finaldata = {'Projects': append_json}
+#         finaldata = {'Projects': append_json}
 
-        print(finaldata)
-        return finaldata
-    except:
-        print("Database Error4")
+#         print(finaldata)
+#         return finaldata
+#     except:
+#         print("Database Error4")
 
 def returnProjects():
     try:
@@ -190,6 +190,8 @@ def returnExpenseitems():
 
         append_json = []
         for row in jsondata:
+            row[3] = row[3].replace("\\", "")
+            row[4] = row[4].replace("\\", "")
             jsondata1 = {"id":row[0], "group":row[1], "title":row[2], "start_time":row[3], "end_time":row[4], "bgColor":row[5], "expense":row[6], "description":row[7], "isWhatIF":row[8]}
             append_json.append(jsondata1)
         ## jsondata = {'ExpenseGroups': jsondata}
@@ -203,7 +205,7 @@ def returnExpenseitems():
         print(finaldata)
         return finaldata
     except:
-        print("Database Error4")
+        print("Database Error5")
 
 #returnprojects(10002)
 
