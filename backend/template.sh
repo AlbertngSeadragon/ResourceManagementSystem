3a33507b67076262d82da86d02e3192057f975c7
albertng <albertng412@gmail.com>
2022-02-21 22:39:56 +0800

    Python auto deploying setup

---
+++ backend/template.sh
@@ -0,0 +1,13 @@
+sudo apt-get update
+
+sudo apt-get install python3
+
+sudo apt-get install python3-pip
+
+Y
+
+git clone --branch feature/add-preview-functionv2 https://github.com/AlbertngSeadragon/ResourceManagementSystem
+
+cd ResourceManagementSystem/backend/
+
+pip install -r requirements.txt
