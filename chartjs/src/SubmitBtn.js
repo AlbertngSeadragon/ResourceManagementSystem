229a7bf386e4bc02ea74d5f10b518e1dfebba57f
albertng <albertng412@gmail.com>
2022-02-26 18:55:58 +0800

    Update the Data will have loading

--- chartjs/src/SubmitBtn.js
+++ chartjs/src/SubmitBtn.js
@@ -1,20 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { updateProjects } from "./services/functions/projectsQuery";
 import { updateExpenseItems } from "./services/functions/expenseItemsQuery";
 import { updateExpenseGroups } from "./services/functions/expenseGroupsQuery";
+import LoadingButton from "@mui/lab/LoadingButton";
+import SendIcon from "@mui/icons-material/Send";
 
-function submitBtn({ groups, items, projects }) {
+function SubmitBtn({ groups, items, projects }) {
+  const [isLoading, setIsLoading] = useState(false);
   const handleSubmit = async () => {
+    setIsLoading(true);
     await updateExpenseItems(items);
     await updateProjects(projects);
     await updateExpenseGroups(groups);
+    setIsLoading(false);
   };
 
   return (
-    <button onClick={handleSubmit} style={{ width: "300px" }}>
-      Submit
-    </button>
+    <>
+      {
+        <LoadingButton
+          onClick={handleSubmit}
+          endIcon={<SendIcon />}
+          loading={isLoading}
+          style={{ width: "300px" }}
+          loadingPosition="end"
+          variant="contained"
+        >
+          Submit
+        </LoadingButton>
+      }
+    </>
   );
 }
 
-export default submitBtn;
+export default SubmitBtn;
